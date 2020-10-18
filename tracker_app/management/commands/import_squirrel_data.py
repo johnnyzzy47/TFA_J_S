8d643dd81ebade0d85e3a0c57416f0f8548d8ead
johnnyzzy47 <johnny.zhou47@gmail.com>
2020-10-18 22:26:01 +0000

    updated model

--- tracker_app/management/commands/import_squirrel_data.py
+++ tracker_app/management/commands/import_squirrel_data.py
@@ -17,3 +17,33 @@ class Command(BaseCommand):
             path = kwargs['path']
             msg = f'You are importing'
             self.stdout.write(self.style.SUCCESS(msg))
+            with open(path, 'rt') as f:
+                reader = csv.DictReader(f)
+                data = list(reader)
+                for row in data:
+                    squirrel = Squirrel(
+                            X=row['\ufeffX'],
+                            Y=row['Y'],
+                            UID=row['Unique Squirrel ID'],
+                            Shift=row['Shift'],
+                            Date=row['Date'],
+                            Age=row['Age'],
+                            Primary_Fur_Color=row['Primary Fur Color'],
+                            Location=row['Location'],
+                            Specific_Location=row['Specific Location'],
+                            Running=strtobool(row['Running']),
+                            Chasing=strtobool(row['Chasing']),
+                            Climbing=strtobool(row['Climbing']),
+                            Eating=strtobool(row['Eating']),
+                            Foraging=strtobool(row['Foraging']),
+                            Other_activities=row['Other Activities'],
+                            Kuks=strtobool(row['Kuks']),
+                            Quaas=strtobool(row['Quaas']),
+                            Moans=strtobool(row['Moans']),
+                            Tail_flags=strtobool(row['Tail flags']),
+                            Tail_twitches=strtobool(row['Tail twitches']),
+                            Approaches=strtobool(row['Approaches']),
+                            Indifferent=strtobool(row['Indifferent']),
+                            Runs_From=strtobool(row['Runs from']),
+                            )
+                        squirrel.save()
